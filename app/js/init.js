3c89251859bd5ed4c9006c4f54d4076ca5969fb3
Olga Grichanovskaya <olga.grichanovskaya@gmail.com>
2018-06-13 22:35:56 +0300

    add range into aside-section

---
+++ app/js/init.js
@@ -0,0 +1,12 @@
+;(function($) {
+    $("#price-range").ionRangeSlider({
+        type: "double",
+        grid: false,
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 800,
+        prefix: "$",
+        hide_min_max: true
+    });
+})(jQuery);
